bf5cea136fadf7beee59adde68bd6735ef7f2aa8
Mathieu Briand Beaulieu <eredian@gmail.com>
2015-05-23 18:38:29 -0400

    Multiple small fixes and removal of require.js to simplify build

--- scripts/hexmapper.ts
+++ scripts/hexmapper.ts
@@ -1,6 +1,4 @@
-/// <reference path="utils/domready.d.ts" />
-
-import utils = require('utils/urlObject');
+/// <reference path="utils/urlobject.ts" />
 
 enum ColumnPosition {
     LEFT,
@@ -20,21 +18,25 @@ enum Mode {
     EXPLORE
 }
 
-export class HexMap {
+class HexMap {
 
-    hexSideLength: number = 40;
-    hexWidth: number = Math.round(Math.sqrt(3) * this.hexSideLength);
-    hexHeight: number = 2 * this.hexSideLength;
+    hexSideLength: number;
+    strokeLineWidth: number;
+    hexWidth: number;
+    hexHeight: number;
+    baseXPos: number = 0;
+    baseYPos: number = 0;
+    
     selectedHex: string = "";
     defaultMapSize: number = 50;
+    zoomLevel: number = 1;
     defaultMapColor: TileColor = TileColor.CAVERN_GROUND;
     selectedColor: TileColor = this.defaultMapColor;
     defaultMapTile: string = "nothing";
     mode: Mode = Mode.EXPLORE;
     mapTiles: HexMapTiles = new HexMapTiles();
     colorMap: { [key: number]: string } = {};
-    baseXPos = this.hexWidth / 2 + 3;
-    baseYPos = this.hexHeight / 2 + 3;
+    zoomLevelMap: {[key: number]: [number, number]} = {};
     
     url = utils.urlObject({});
 
@@ -42,20 +44,58 @@ export class HexMap {
     canvas = <HTMLCanvasElement> document.getElementById("canvas");
     context = <CanvasRenderingContext2D> this.canvas.getContext("2d");
 
+    mouseHeld: boolean = false;
+
     constructor() {
         this.colorMap[TileColor.CAVERN_GROUND] = "#B4B4B4";
         this.colorMap[TileColor.NOTHING] = "#000000";
         this.colorMap[TileColor.MURKY_WATER] = "#1919AA";
         this.colorMap[TileColor.SWAMP_RIVER] = "#2D4BC8";
         this.colorMap[TileColor.LAVA] = "#FF3714";
-
+        
+        this.zoomLevelMap[0] = [6,1];
+        this.zoomLevelMap[1] = [35,3];
+        this.zoomLevelMap[2] = [80,5];
+        
         if (this.url["parameters"]["mode"] == "edit") {
             this.mode = Mode.EDIT;
         } else {
             this.mode = Mode.EXPLORE;
         }
         
+        this.updateHexDimensions(1);
+        
         this.loadImages();
+        
+        this.canvas.addEventListener("click", (e) => this.click(e));
+        this.canvas.addEventListener("mousedown", (e) => this.click(e));
+        this.canvas.addEventListener("mouseup", (e) => this.click(e));
+        this.canvas.addEventListener("mousemove", (e) => this.click(e));
+    }
+    
+    updateHexDimensions(zoomLevel: number) {
+        this.hexSideLength = this.zoomLevelMap[zoomLevel][0];
+        this.strokeLineWidth = this.zoomLevelMap[zoomLevel][1];
+        this.hexWidth = Math.round(Math.sqrt(3) * this.hexSideLength);
+        this.hexHeight = 2 * this.hexSideLength;
+    }
+    
+    zoomIn() {
+        this.baseXPos *= this.zoomLevelMap[this.zoomLevel+1][0]/this.zoomLevelMap[this.zoomLevel][0];
+        this.baseYPos *= this.zoomLevelMap[this.zoomLevel+1][0]/this.zoomLevelMap[this.zoomLevel][0];
+        this.zoomLevel++;
+        this.updateHexDimensions(this.zoomLevel);
+        this.deleteMap();
+        this.drawMap();
+    }
+    
+    zoomOut() {
+        this.baseXPos *= this.zoomLevelMap[this.zoomLevel-1][0]/this.zoomLevelMap[this.zoomLevel][0];
+        this.baseYPos *= this.zoomLevelMap[this.zoomLevel-1][0]/this.zoomLevelMap[this.zoomLevel][0];
+        this.zoomLevel--;
+        this.updateHexDimensions(this.zoomLevel);
+        this.deleteMap();
+        this.drawMap();
     }
     
     loadImages() {
@@ -91,9 +131,10 @@ export class HexMap {
 
     drawTile(tile: HexTile) {
         var explored = tile["explored"] || this.mode == Mode.EDIT;
-        //stringOutput += '<div id="' + tile["x"] + ',' + tile["y"] + '" class="hexcell display ' + tile["tile"] + ' ' + tile["color"] + '" onmousemove="map.hexHovered(this.id)" onClick="map.hexClicked(this.id)">';
+        
         var XPos = this.baseXPos + this.hexWidth * (tile.x + tile.y / 2);
         var YPos = this.baseYPos + this.hexSideLength * tile.y * 1.5;
+        
         this.context.beginPath();
         this.context.moveTo(XPos, YPos - this.hexSideLength);
         this.context.lineTo(XPos + this.hexWidth / 2, YPos - this.hexSideLength / 2);
@@ -103,15 +144,19 @@ export class HexMap {
         this.context.lineTo(XPos - this.hexWidth / 2, YPos - this.hexSideLength / 2);
         this.context.lineTo(XPos, YPos - this.hexSideLength);
         this.context.lineTo(XPos + this.hexWidth / 2, YPos - this.hexSideLength / 2);
-
-        this.context.fillStyle = this.colorMap[tile.color];
-        this.context.lineWidth = 1;
+        
+        if(explored) {
+            this.context.fillStyle = this.colorMap[tile.color];
+        } else {
+            this.context.fillStyle = "#FFCC66";
+        }
+        this.context.lineWidth = this.strokeLineWidth;
         this.context.strokeStyle = 'black';
         this.context.fill();
         this.context.stroke();
         this.context.closePath();
         
-        if(tile.image != "nothing" && tile.image != "") {
+        if(tile.image != "nothing" && tile.image != "" && this.hexSideLength > 20 && explored) {
             var baseImage = this.baseImages[tile.image];
             this.context.drawImage(baseImage, XPos - baseImage.width/2, YPos - baseImage.height/2);
         }
@@ -199,9 +244,13 @@ export class HexMap {
     }
 
     load() {
+        var mapName: string = prompt("Enter map name.");
+        if (mapName == null) {
+            return;
+        }
         this.loadImages();
         var xmlhttp = new XMLHttpRequest();
-        var url = "load.php?filename=" + (<HTMLTextAreaElement>document.getElementById("mapData")).value;
+        var url = "load.php?filename=" + mapName;
 
         var parent = this;
         xmlhttp.onreadystatechange = function() {
@@ -216,9 +265,13 @@ export class HexMap {
     }
 
     save() {
+        var mapName: string = prompt("Enter map name.");
+        if (mapName == null) {
+            return;
+        }
         var xmlhttp = new XMLHttpRequest();
-        var url = "save.php?filename=" + (<HTMLTextAreaElement>document.getElementById("mapData")).value;
-
+        var url = "save.php?filename=" + mapName;
+        
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 alert(xmlhttp.responseText);
@@ -230,29 +283,70 @@ export class HexMap {
     }
     
     up() {
-        this.baseYPos += 600;
+        this.baseYPos += 374;
         this.deleteMap();
         this.drawMap();
     }
     
     down() {
-        this.baseYPos -= 600;
+        this.baseYPos -= 374;
         this.deleteMap();
         this.drawMap();
     }
     
     left() {
-        this.baseXPos += 800;
+        this.baseXPos += 512;
         this.deleteMap();
         this.drawMap();
     }
     
     right() {
-        this.baseXPos -= 800;
+        this.baseXPos -= 512;
         this.deleteMap();
         this.drawMap();
     }
-
+    
+    pixel_to_hex(x: number, y: number) {
+        var q = (x * Math.sqrt(3)/3 - y / 3) / this.hexSideLength;
+        var r = y * 2/3 / this.hexSideLength;
+        var hexPosition = this.hex_round(q, r);
+        return this.mapTiles.get(hexPosition[0], hexPosition[1]);
+    }
+    
+    hex_round(q: number, r: number) {
+        var x: number = q;
+        var z: number = r;
+        var y: number = -x-z;
+        
+        var rx: number = Math.round(x);
+        var ry: number = Math.round(y);
+        var rz: number = Math.round(z);
+    
+        var x_diff: number = Math.abs(rx - x);
+        var y_diff: number = Math.abs(ry - y);
+        var z_diff: number = Math.abs(rz - z);
+    
+        if (x_diff > y_diff && x_diff > z_diff) {
+            rx = -ry-rz;
+        } else if (y_diff > z_diff) {
+            ry = -rx-rz;
+        } else {
+            rz = -rx-ry;
+        }
+        return [rx, rz];
+    }
+    
+    click(e: MouseEvent) {
+        if(e.type == "mousedown") {
+            this.mouseHeld = true;
+        } else if(e.type == "mouseup") {
+            this.mouseHeld = false;
+        }
+        if(e.type != "mousemove" || this.mouseHeld == true) {
+            var hex: HexTile = this.pixel_to_hex(e.clientX-this.baseXPos, e.clientY-this.baseYPos);
+            this.mapTiles.forEachInFOV(hex.x, hex.y, (e) => this.explore(e));
+        }
+    }
 }
 
 class HexMapTiles {
@@ -322,16 +416,46 @@ document.ontouchmove = function(event) {
 //domready(function() {
 
 
-document.addEventListener('touchend', function(e) {
-  e.preventDefault();
-  var event = document.createEvent('Events');
-  event.initEvent('onmouseup', true, false);
-  var event2 = document.createEvent('Events');
-  event2.initEvent('click', true, false);
-  e.srcElement.dispatchEvent(event);
-  e.srcElement.dispatchEvent(event2);
-})
+document.addEventListener("touchstart", function(e: TouchEvent) {
+    e.preventDefault();
+    var touches = e.changedTouches;
+    
+    for (var i=0; i < touches.length; i++) {
+        var event: MouseEvent = new MouseEvent("onmousedown", {clientX:touches[i].clientX,clientY:touches[i].clientY});
+        event.initEvent('mousedown', true, false);
+        e.target.dispatchEvent(event);
+    }
+});
+document.addEventListener('touchend', function(e: TouchEvent) {
+    e.preventDefault();
+    var touches = e.changedTouches;
+  
+    for (var i=0; i < touches.length; i++) {
+        /*var event: MouseEvent = new MouseEvent("onmouseup", {clientX:touches[i].clientX,clientY:touches[i].clientY});
+        event.initEvent('mouseup', true, false);
+        e.target.dispatchEvent(event);*/
+        var event2: MouseEvent = new MouseEvent("onclick", {clientX:touches[i].clientX,clientY:touches[i].clientY});
+        event2.initEvent('click', true, false);
+        e.target.dispatchEvent(event2);
+    }
+});
+//document.addEventListener("touchcancel", handleCancel, false);
+//document.addEventListener("touchleave", handleEnd, false);
+document.addEventListener("touchmove", function(e: TouchEvent) {
+    e.preventDefault();
+    var touches = e.changedTouches;
+  
+    for (var i=0; i < touches.length; i++) {
+        var event: MouseEvent = new MouseEvent("onmousemove", {clientX:touches[i].clientX,clientY:touches[i].clientY});
+        event.initEvent('mousemove', true, false);
+        e.target.dispatchEvent(event);
+    }
+});
+
+window.onload = function() {
     var map = new HexMap();
+    document.getElementById("zoomInButton").addEventListener("click", () => map.zoomIn());
+    document.getElementById("zoomOutButton").addEventListener("click", () => map.zoomOut());
     document.getElementById("mapSaveButton").addEventListener("click", () => map.save());
     document.getElementById("mapLoadButton").addEventListener("click", () => map.load());
     
@@ -339,8 +463,21 @@ document.addEventListener('touchend', function(e) {
     document.getElementById("downButton").addEventListener("click", () => map.down());
     document.getElementById("leftButton").addEventListener("click", () => map.left());
     document.getElementById("rightButton").addEventListener("click", () => map.right());
-
+    
     map.generateNewDefaultMap();
     map.drawMap();
+    
+    window.addEventListener('keydown', function(event : KeyboardEvent) {
+      switch (event.key) {
+        case "q": // Left
+          //Game.player.moveLeft();
+        break;
+    
+        case "a": // Up
+          //Game.player.moveUp();
+        break;
+      }
+    }, false);
+}
 //});
 
--- scripts/utils/urlobject.ts
+++ scripts/utils/urlobject.ts
@@ -1,78 +1,80 @@
-export function urlObject(options) {
-    var url_search_arr;
-    var option_key;
-    var i;
-    var urlObj;
-    var get_param;
-    var key;
-    var val;
-    var url_query;
-    var url_get_params = {};
-    var a = document.createElement('a');
-    var default_options = {
-            'url': window.location.href,
-            'unescape': true,
-            'convert_num': true
+module utils {
+    export function urlObject(options) {
+        var url_search_arr;
+        var option_key;
+        var i;
+        var urlObj;
+        var get_param;
+        var key;
+        var val;
+        var url_query;
+        var url_get_params = {};
+        var a = document.createElement('a');
+        var default_options = {
+                'url': window.location.href,
+                'unescape': true,
+                'convert_num': true
+            };
+    
+        if (typeof options !== "object") {
+            options = default_options;
+        } else {
+            for (option_key in default_options) {
+                if (default_options.hasOwnProperty(option_key)) {
+                    if (options[option_key] === undefined) {
+                        options[option_key] = default_options[option_key];
+                    }
+                }
+            }
+        }
+    
+        a.href = options.url;
+        url_query = a.search.substring(1);
+        url_search_arr = url_query.split('&');
+    
+        if (url_search_arr[0].length > 1) {
+            for (i = 0; i < url_search_arr.length; i += 1) {
+                get_param = url_search_arr[i].split("=");
+    
+                if (options.unescape) {
+                    key = decodeURI(get_param[0]);
+                    val = decodeURI(get_param[1]);
+                } else {
+                    key = get_param[0];
+                    val = get_param[1];
+                }
+    
+                if (options.convert_num) {
+                    if (val.match(/^\d+$/)) {
+                        val = parseInt(val, 10);
+                    } else if (val.match(/^\d+\.\d+$/)) {
+                        val = parseFloat(val);
+                    }
+                }
+    
+                if (url_get_params[key] === undefined) {
+                    url_get_params[key] = val;
+                } else if (typeof url_get_params[key] === "string") {
+                    url_get_params[key] = [url_get_params[key], val];
+                } else {
+                    url_get_params[key].push(val);
+                }
+    
+                get_param = [];
+            }
+        }
+    
+        urlObj = {
+            protocol: a.protocol,
+            hostname: a.hostname,
+            host: a.host,
+            port: a.port,
+            hash: a.hash.substr(1),
+            pathname: a.pathname,
+            search: a.search,
+            parameters: url_get_params
         };
-
-    if (typeof options !== "object") {
-        options = default_options;
-    } else {
-        for (option_key in default_options) {
-            if (default_options.hasOwnProperty(option_key)) {
-                if (options[option_key] === undefined) {
-                    options[option_key] = default_options[option_key];
-                }
-            }
-        }
+    
+        return urlObj;
     }
-
-    a.href = options.url;
-    url_query = a.search.substring(1);
-    url_search_arr = url_query.split('&');
-
-    if (url_search_arr[0].length > 1) {
-        for (i = 0; i < url_search_arr.length; i += 1) {
-            get_param = url_search_arr[i].split("=");
-
-            if (options.unescape) {
-                key = decodeURI(get_param[0]);
-                val = decodeURI(get_param[1]);
-            } else {
-                key = get_param[0];
-                val = get_param[1];
-            }
-
-            if (options.convert_num) {
-                if (val.match(/^\d+$/)) {
-                    val = parseInt(val, 10);
-                } else if (val.match(/^\d+\.\d+$/)) {
-                    val = parseFloat(val);
-                }
-            }
-
-            if (url_get_params[key] === undefined) {
-                url_get_params[key] = val;
-            } else if (typeof url_get_params[key] === "string") {
-                url_get_params[key] = [url_get_params[key], val];
-            } else {
-                url_get_params[key].push(val);
-            }
-
-            get_param = [];
-        }
-    }
-
-    urlObj = {
-        protocol: a.protocol,
-        hostname: a.hostname,
-        host: a.host,
-        port: a.port,
-        hash: a.hash.substr(1),
-        pathname: a.pathname,
-        search: a.search,
-        parameters: url_get_params
-    };
-
-    return urlObj;
 }
